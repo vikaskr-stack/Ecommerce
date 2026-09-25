d5a92482e8ed99437788de6694c38dd9c6dd15cd
vikas <vikaskr13062007@gmail.com>
2026-09-25 19:44:08 +0530

    Allow Render frontend host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,10 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import taiwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),taiwindcss()],
-})
+  plugins: [react()],
+
+  preview: {
+    allowedHosts: ["ecommerce-frontend-ealq.onrender.com"],
+  },
+});
